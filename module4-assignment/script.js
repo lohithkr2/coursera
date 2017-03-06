17ee88d6bb82aeff6c8aec7c6860407c83e83b01
lohith KR <lohith KR>
2017-03-06 12:30:53 +0530

    module4-assignment

---
+++ module4-assignment/script.js
@@ -0,0 +1,15 @@
+﻿// JavaScript Document
+
+(function () {
+
+var names = ["Yasier", "Jenny", "Johney", "Jason", "Paul", "Farahan", "Larry", "Pavith", "Latha", "Jagga" , "Jagadesh"];
+
+for(var i=0;i < names.length;i++){
+if(names[i].charAt(0) == "J" || names[i].charAt(0) == "j"){
+	console.log("Hello "+names[i]);
+}else{
+	console.log("Good Bye "+names[i]);
+}
+}
+
+})();
